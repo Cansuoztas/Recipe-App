2d9e2b8f4cbec91e2252b0a652b2f8403d533f46
Cansu <109246701+Cansuoztas@users.noreply.github.com>
2022-12-05 10:45:34 +0300

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import yemekLogo from "../../assets/logo.png"
-import NavbarDiv, { NavbarUl } from './Navbar.style'
+import NavbarDiv, { NavbarUl } from './Navbar.Style'
 
 const Navbar = () => {
   return (
